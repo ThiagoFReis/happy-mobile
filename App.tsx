3958166fef59713872e9f74c1cd82aca9b6916df
Thiago <thiagofreitas201717@gmail.com>
2020-10-19 23:00:29 -0300

    mobile project started

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider= { PROVIDER_GOOGLE } 
+        style={styles.map} 
+        initialRegion={{
+          latitude: -15.8618931,
+          longitude: -47.9515105,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          coordinate={{
+            latitude: -15.8618931,
+            longitude: -47.9515105,
+          }}
+        >
+          <Callout>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker> 
+      </MapView>
     </View>
   );
 }
@@ -14,8 +39,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     justifyContent: 'center',
+  },
+
+  calloutText: {
+
   },
 });
